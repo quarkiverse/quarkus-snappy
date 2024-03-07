a999ef37f6c807024d9fc8f88765ad3f6d2ffcc4
MarcAnB <marc.an.bea@gmail.com>
2024-03-07 13:35:55 -0800

    load extension config

--- deployment/src/main/java/io/quarkiverse/snappy/deployment/SnappyBuildTimeConfig.java
+++ deployment/src/main/java/io/quarkiverse/snappy/deployment/SnappyBuildTimeConfig.java
@@ -1,7 +1,10 @@
 package io.quarkiverse.snappy.deployment;
 
 import io.quarkus.runtime.annotations.ConfigItem;
+import io.quarkus.runtime.annotations.ConfigPhase;
+import io.quarkus.runtime.annotations.ConfigRoot;
 
+@ConfigRoot(name = "kafka", phase = ConfigPhase.BUILD_TIME)
 public class SnappyBuildTimeConfig {
 
     @ConfigItem(name = "snappy.enabled", defaultValue = "false")
